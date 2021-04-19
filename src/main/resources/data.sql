e4d1116c22f384368cd85eef43580a97db3a04c3
Gadelha <gadelha.ti@gmail.com>
2021-04-19 09:48:42 -0300

    Retirando constraint.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,11 +3,11 @@
 CREATE TABLE tutorials (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   title VARCHAR(250) NOT NULL,
-  description VARCHAR(250) NOT NULL,
+  description VARCHAR(250),
   published VARCHAR(250) DEFAULT NULL
 );
 
 INSERT INTO tutorials (id, title, description, published) VALUES
-  (1, 'Um', 'Alpha', true),
-  (2, 'Dois', 'Bravo', true),
-  (3, 'Três', 'Charlie', true);
+  (14546345, 'Um', 'Alpha', true),
+  (45134564, 'Dois', 'Bravo', true),
+  (45643645, 'Três', 'Charlie', true);
