06b2d5abcba975ab6a6d3799750f01f9b82a293b
Gadelha <gadelha.ti@gmail.com>
2021-04-21 07:15:42 -0300

    Enrriquecendo o código.

--- src/main/java/br/eti/gadelha/controller/TutorialControllerThymeleaf.java
+++ src/main/java/br/eti/gadelha/controller/TutorialControllerThymeleaf.java
@@ -1,14 +1,12 @@
 package br.eti.gadelha.controller;
 
+import br.eti.gadelha.persistence.dto.request.DTORequestTutorial;
 import br.eti.gadelha.persistence.model.Tutorial;
 import br.eti.gadelha.service.ServiceTutorial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -22,8 +20,9 @@ import javax.validation.constraints.NotBlank;
  * @link	www.gadelha.eti.br
  **/
 
-@Controller
+@RestController
 @RequestMapping(value= "/tutorialMV")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class TutorialControllerThymeleaf {
 
     private final ServiceTutorial serviceTutorial;
@@ -39,6 +38,14 @@ public class TutorialControllerThymeleaf {
             return add(tutorial);
         }
         serviceTutorial.create(tutorial);
+        return findAll();
+    }
+    @PostMapping("/createDTO")
+    public ModelAndView create(@Valid DTORequestTutorial tutorial, BindingResult bindingResult) {
+        Tutorial tutorial1 = serviceTutorial.create(tutorial.toObject());
+        if(bindingResult.hasErrors()) {
+            return add(tutorial1);
+        }
         return findAll();
     }
     @GetMapping("/retrieve")
--- src/main/java/br/eti/gadelha/persistence/repository/TutorialRepository.java
+++ src/main/java/br/eti/gadelha/persistence/repository/TutorialRepository.java
@@ -16,6 +16,6 @@ import java.util.List;
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
     List<Tutorial> findByTitleContaining(String title);
-
+    List<Tutorial> findAllByOrderByTitleDesc();
     boolean existsByTitle(String title);
 }
